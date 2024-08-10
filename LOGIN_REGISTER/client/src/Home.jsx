f025c3d48ebdd203bdfaed49d2a97cddb1b2b184
ra-iy-ra <BotxSoftwareProject102@gmail.com>
2024-08-10 18:03:04 +0530

    Add files via upload

---
+++ LOGIN_REGISTER/client/src/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+ return (
+    <div className="center">
+      <h1>WELCOME TO HOME PAGE BRO!!!!!</h1>
+      <img
+        src="https://t3.ftcdn.net/jpg/02/80/01/64/360_F_280016442_I5DcWCRT7JTr5Ut86a9VvqNoOfDt854G.jpg"
+        alt="car"
+      />
+    </div>
+ );
+}
+
+export default Home;
